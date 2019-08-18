4bc39513612c00230490f4d52b135675ba06f6c6
Jocelyn Casteletti <jocelyn.casteletti@gmail.com>
2019-08-18 18:45:13 -0400

    mapa ocupa alto restante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { HeaderComponent } from './components/header/header.component';
 import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDjPJXYoyNAMKnxzoNWPQGC4HhtdeiZzrs'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
